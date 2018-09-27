ca09d72a4cfbc086685325bf2510818a75981ecd
aljarrell <alan.k.jarrell@gmail.com>
2018-09-27 16:22:02 -0400

    added spacing

--- test_fizz.rb
+++ test_fizz.rb
@@ -5,24 +5,31 @@ class My_test < Minitest::Test
     def test_number_one
         assert_equal(1,1)
     end
+
     def test_two
       assert_equal(2,mmkata(2))
     end
+
     def test_three
       assert_equal("mined",mmkata(3))
     end
+
     def test_four
         assert_equal("mined_minds",fizz(15))
     end
+
     def test_five
         assert_equal("mined",fizz(6))
     end
+
     def test_six
         assert_equal("minds",fizz(10))
     end
+
     def test_seven
       assert_equal("buzz",fizz(4))
     end
+
     def test_eight
       assert_equal("buzz",fizz(7))
     end
